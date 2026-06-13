b4313f2e345e89239664bbb9bfe21d1b95e822a7
Coverst <206331970+Coverst-ux@users.noreply.github.com>
2026-06-13 14:25:02 +0300

    feat: add check_and_merge function for body collisions

--- src/core/physics.py
+++ src/core/physics.py
@@ -1,4 +1,5 @@
 # src/core/physics.py
+from src.core.body import Body
 from src.utils.constants import EPSILON
 from src.utils.constants import G
 from src.utils.vector import Vector2D
@@ -18,3 +19,22 @@ def gravitational_force_softened(m1, m2, pos1, pos2):
     distance_sq = displacement.x**2 + displacement.y**2 + EPSILON**2
     magnitude = G * m1 * m2 / distance_sq
     return displacement.normalized() * magnitude
+
+def check_and_merge(bodies: list[Body], merge_radius: float) -> list[Body]:
+    merged = set()
+    new_bodies = []
+    for i, b1 in enumerate(bodies):
+        if i in merged:
+            continue
+        for j, b2 in enumerate(bodies):
+            if j <= i or j in merged:
+                continue
+            distance = (b2.position - b1.position).magnitude() 
+            if distance < merge_radius:
+                new_mass = b2.mass + b1.mass
+                new_velocity = (b2.mass * b2.velocity + b1.mass * b1.velocity) * (1.0 /new_mass)
+                b1.mass = new_mass
+                b1.velocity = new_velocity
+                merged.add(j)
+        new_bodies.append(b1)
+    return new_bodies
