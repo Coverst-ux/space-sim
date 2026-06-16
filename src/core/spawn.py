57603837304a436e0b4fc911eb1f82529c8f529e
Coverst <206331970+Coverst-ux@users.noreply.github.com>
2026-06-16 11:55:43 +0300

    feat: add radius-based color gradient to N-body bodies

--- src/core/spawn.py
+++ src/core/spawn.py
@@ -17,18 +17,22 @@ def generate_random_bodies(n:int)-> list[Body]:
     bodies.append(core)
 
     for i in range(n):
-        angle = random.uniform(0, 2 * math.pi) 
+        angle = random.uniform(0, 2 * math.pi)
         r = random.uniform(1e11, 1e12)
         x = r * math.cos(angle)
         y = r * math.sin(angle)
         v_circ = math.sqrt(G * core_mass / r)
         vx = -v_circ * math.sin(angle)  # perpendicular to position
         vy =  v_circ * math.cos(angle)
+        r_norm = (r - 1e11) / (1e12 - 1e11)
+        red = int(255 * (1 - r_norm))
+        blue = int(255 * r_norm)
         new_body = Body(
             name=f"body_{i}",
             mass=SOLAR_MASS,
             position=Vector2D(x, y),
-            velocity=Vector2D(vx, vy)
+            velocity=Vector2D(vx, vy),
+            color=(red,100,blue)
         )
         bodies.append(new_body)
 
